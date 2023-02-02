863c0b250dfb32cfdac444f8e097a2cbfd90d56b
miftahfaridhh <muhammadmiftahfaridh@gmail.com>
2023-02-02 22:14:17 +0900

    no changes

--- arp.py
+++ arp.py
@@ -5,4 +5,4 @@ import os
 log = time.strftime("/home/pi/Int3rcept0r/arp/log/%Y%m%d-%H%M%S") 
 subprocess.Popen(["sudo","ettercap","-Tq","-M","arp","-i","eth0","-L", log])
 time.sleep(10)
-os.system('sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"')
+os.system('sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"') 
